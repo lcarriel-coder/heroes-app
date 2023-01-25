62e2ef25425db3359110385b7caacba6b3b79d2c
Luis Carriel <imagetech.developer11@outlook.com>
2023-01-25 00:03:02 -0500

    publicroute-and-lastpage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,24 +3,23 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
-
-  const { login } = useContext( AuthContext );
+  const { login } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
   const onLogin = () => {
 
-    login( 'Fernando Herrera' );
-    navigate("/", { replace: true });
-  };
+    const lastPath = localStorage.getItem("lastPath") || "/";
 
-  
+    login("Fernando Herrera");
+    navigate(lastPath, { replace: true });
+  };
 
   return (
     <div className="container mt-5">
       <h1>Login</h1>
       <hr />
-      <button className="btn btn-primary" onClick={ onLogin }>
+      <button className="btn btn-primary" onClick={onLogin}>
         Login
       </button>
     </div>
